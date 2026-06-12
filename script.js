f9be86c17642cab6ec15cf30a37972e51ce24128
raihanabdul71 <raihanabiola@gmail.com>
2026-06-12 15:53:21 -0700

    Game Project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Declare variables below to save the different divs of your story.
-
-
+let optionOneButton = document.querySelector('.option-one');
+let optionOneScreen = document.querySelector('.option-one-screen');
+let opening = document.querySelector('.story-opening');
 
 
 
@@ -12,9 +13,13 @@
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionOneButton.addEventListener ('click', function(){
+    optionOneButton.style.display = 'none';
+    optionOneScreen.style.display = 'block';
+    opening.style.display = 'none';
 
-// });
+
+});
 
 // INSERT_VARIABLE.addEventListener('click', function(){
 
