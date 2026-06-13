e8440bd25b39e6693bcf3af062af67cb57ec8574
raihanabdul71 <raihanabiola@gmail.com>
2026-06-13 00:30:10 -0700

    Game project

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Declare variables below to save the different divs of your story.
-let optionOneButton = document.querySelector('.option-one');
+
+// Screen 1
+let optionOneBtn = document.querySelector('.option-one');
 let optionOneScreen = document.querySelector('.option-one-screen');
 let opening = document.querySelector('.story-opening');
+let subTitle = document.querySelector('.sub-title');
+let firstButtons = document.querySelector('.buttons')
+let optionOneTitle = document.querySelector('.option-one-title');
+
+// Screen 2
+let optionTwoBtn = document.querySelector('.option-two');
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let optionTwoTitle = document.querySelector('.option-two-title')
+let secondBtns = document.querySelector('.option-one-end-btn');
+
+// Screen 3
+let optionOneEndTitle = document.querySelector('.option-one-end-title');
+let optionOneEnd = document.querySelector('.option-one-end');
+let thirdBtns = document.querySelector('.option-two-end-btn');
+
+
+// Screen 4
+let optionTwoEndTitle = document.querySelector('.option-two-end-title');
+let optionTwoEnd = document.querySelector('.option-two-end');
 
 
 
@@ -13,19 +34,65 @@ let opening = document.querySelector('.story-opening');
 //  - Then fill in the blanks with the correct variables.
 
 
-optionOneButton.addEventListener ('click', function(){
-    optionOneButton.style.display = 'none';
+optionOneBtn.addEventListener ('click', function(){
+    optionOneTitle.style.display = 'block';
     optionOneScreen.style.display = 'block';
     opening.style.display = 'none';
+    subTitle.style.display = 'none';
+    firstButtons.style.display = 'none';
+    optionTwoScreen.style.display = 'none'
+    optionTwoTitle.style.display = 'none';
+    optionOneEndTitle.style.display = 'none';
+    optionOneEnd.style.display = 'none';
+    optionTwoEndTitle.style.display = 'none';
+    optionTwoEnd.style.display = 'none';
 
 
 });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionTwoBtn.addEventListener('click', function(){
+    optionTwoScreen.style.display = 'block';
+    optionTwoTitle.style.display = 'block';
+    opening.style.display = 'none';
+    subTitle.style.display = 'none';
+    firstButtons.style.display = 'none';
+    optionOneScreen.style.display = 'none';
+    optionOneTitle.style.display = 'none';
+    optionOneEndTitle.style.display = 'none';
+    optionOneEnd.style.display = 'none';
+    optionTwoEndTitle.style.display = 'none';
+    optionTwoEnd.style.display = 'none';
 
-// });
+});
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+secondBtns.addEventListener('click', function(){
+    optionOneEndTitle.style.display = 'block';
+    optionOneEnd.style.display = 'block';
+    optionOneTitle.style.display = 'none';
+    optionOneScreen.style.display = 'none';
+    optionTwoTitle.style.display = 'none';
+    optionTwoScreen.style.display = 'none';
+    opening.style.display = 'none';
+    subTitle.style.display = 'none';
+    firstButtons.style.display = 'none';
+    optionTwoEndTitle.style.display = 'none';
+    optionTwoEnd.style.display = 'none';
+    
+});
 
-// });
+
+thirdBtns.addEventListener('click', function(){
+    optionTwoEndTitle.style.display = 'block';
+    optionTwoEnd.style.display = 'block';
+    optionOneEndTitle.style.display = 'none';
+    optionOneEnd.style.display = 'none';
+    optionOneScreen.style.display = 'none';
+    optionOneTitle.style.display = 'none';
+    optionTwoTitle.style.display = 'none';
+    optionTwoScreen.style.display = 'none';
+    opening.style.display = 'none';
+    subTitle.style.display = 'none';
+    firstButtons.style.display = 'none';
+
+});
